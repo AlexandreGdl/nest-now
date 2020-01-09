93f50445c944c885eb5ffdf20a0cc4fcb3a4965e
Alexandre <alex_didi49@hotmail.com>
2020-01-09 09:49:21 +0100

    :construction: eliott bg adding

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/eliott')
+  getEliott(): string {
+    return this.appService.getEliott();
+  }
 }
